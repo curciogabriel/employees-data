0a3709229a61c00bd74f22f0ebe90647e7246457
curciogabriel <curcioog@gmail.com>
2025-02-13 17:58:24 -0300

    feat: add class employee

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,31 @@
 package application;
 
+import entities.Employee;
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        Employee employee = new Employee();
 
+        System.out.println("Name: ");
+        employee.name = sc.nextLine();
+        System.out.println("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee: " + employee);
+
+        System.out.println();
+        System.out.println("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Updated data: " + employee);
     }
 }
